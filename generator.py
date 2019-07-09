aa15b915e6cfdb86b654045d527ead3f2c0dd614
ostergaardl <l.ostergaard@elsevier.com>
2019-07-09 10:35:24 -0400

    Added CSV export.

--- generator.py
+++ generator.py
@@ -19,6 +19,9 @@ from xml.dom import minidom
 from html import escape
 import dicttoxml
 
+import pandas as pd
+import numpy as np
+
 import click
 
 # Generates persons
@@ -354,10 +357,14 @@ def date_split(date):
 @click.option('--persons_out', default='persons.xml', help='Persons output file.')
 @click.option('--photos/--no-photos', default=False, help='Whether to create photos for persons. Disabled with simple.')
 @click.option('--simple/--complex', default=False, help='Create simple or complex XML output.')
+@click.option('--excel/--no-excel', default=False, help='Output to Excel file.')
 @click.command()
-def main(validate, locale, submission, persons, orgs, orgs_out, persons_out, i_orgs, photos, simple):
+def main(validate, locale, submission, persons, orgs, orgs_out, persons_out, i_orgs, photos, simple, excel):
 	
 	click.echo(click.style("Creating random Pure master data", bold = True) )
+
+	func = transform_to_excel if bool(excel) else transform_to_xml
+
 	# set options
 	cfg = {
 		"validate": validate,
@@ -368,7 +375,8 @@ def main(validate, locale, submission, persons, orgs, orgs_out, persons_out, i_o
 		"o_orgs": orgs_out,
 		"o_persons": persons_out,
 		"simple": bool(simple),
-		"photos": bool(photos)
+		"photos": bool(photos),
+		"transform": func
 	}
 
 	# Mimesis - create provider with same language
@@ -391,7 +399,14 @@ def main(validate, locale, submission, persons, orgs, orgs_out, persons_out, i_o
 
 def generate_orgs(generator, config):
 	result = generator.create_hierarchy(max_children = config["num_orgs"])
-	transform(result, "orgs.xsl", config["o_orgs"], "organisation.xsd", config)
+	config['transform'](
+		data=result, 
+		data_type='organisations', 
+		stylesheet="orgs.xsl", 
+		output_file=config["o_orgs"], 
+		schema_file="organisation.xsd", 
+		config=config)
+
 	return result
 
 def generate_persons(generator, config):
@@ -400,7 +415,14 @@ def generate_persons(generator, config):
 	with click.progressbar(range(0, count), label = 'Creating persons...') as bar:
 		for i in bar:
 			results.append(generator.create(config["simple"], config["photos"]))
-	transform(results, "persons.xsl", config["o_persons"], "person.xsd", config, True)
+	config['transform'](
+		data=results, 
+		data_type='persons', 
+		stylesheet="persons.xsl", 
+		output_file=config["o_persons"], 
+		schema_file="person.xsd", 
+		config=config, 
+		batch = True)
 
 # to reload org IDs from previously generated dataset
 # note: you can use any valid org XML file here
@@ -418,15 +440,55 @@ def to_xml(dict, pretty = False):
 
 	return dicttoxml.dicttoxml(dict, custom_root = 'item', attr_type = False)
 
+# simple func to handle CSV to get raw data - yes arguments could be left out and changed to args... 
+def transform_to_excel(data, output_file, config, data_type = '', stylesheet = '', schema_file = '', batch = False):
 
-def transform(data, stylesheet, output_file, schema_file, config, batch = False):
+	norm = pd.io.json.json_normalize
+
+	writer = pd.ExcelWriter('{0}.xlsx'.format(data_type))
+
+	# normalize all data
+	df_main = norm(data)
+
+	# output to other sheets
+	dataframes = {}
+
+	with click.progressbar(data, label = 'Converting to Excel...') as bar:
+		for entry in bar:
+			stack = list(entry.items())
+			while stack:
+				key, v = stack.pop()
+				if type(v) is not str:
+					if key not in dataframes:
+						dataframes[key] = pd.DataFrame()		
+
+					owner = pd.DataFrame({'{0}_id'.format(data_type):[entry['id']]})
+					normalized = norm(v)
+
+					# combine with the record ID for later lookup
+					df_merged = normalized.assign(t=1).merge(owner.assign(t=1), how = 'outer').drop('t',1)
+
+					dataframes[key] = pd.concat([dataframes[key], df_merged])
+	
+	# remove redundant columns
+	to_drop = [c for c in df_main.columns for k in dataframes.keys() if k in c and k != 'id']
+	df_main.drop(to_drop, axis=1, inplace=True)
+	df_main.to_excel(writer, data_type, index = False)
+	
+	for df_k in dataframes.keys():
+		dataframes[df_k].to_excel(writer, df_k, index=False)
+
+	writer.save()
+	click.echo(click.style("Transformation saved!",fg='green'))
+
+def transform_to_xml(data, data_type, output_file, config, stylesheet, schema_file, batch = False):
 
 	xml_str = ''
 	xml_dom = None
 	# we process persons in batches for better performance
 	if batch:
 		root = ET.Element('root')
-		with click.progressbar(data, label = 'Coverting persons to XML...') as bar:
+		with click.progressbar(data, label = 'Coverting to XML...') as bar:
 			for item in bar:
 				root.append(ET.fromstring(to_xml(item)))
 		xml_dom = root
